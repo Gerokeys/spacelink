0ae8afa9bf35b31ded12fdc04f99021a985a60ff
Gerokeys <shikukugerald111@gmail.com>
2026-05-28 22:38:04 +0300

    Replace logo with tight-cropped version, remove red border

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -24,7 +24,7 @@ export function Navbar() {
             alt="SpaceLink"
             width={220}
             height={80}
-            className="h-20 w-auto border-2 border-red-500 rounded"
+            className="h-20 w-auto"
             priority
           />
         </Link>
